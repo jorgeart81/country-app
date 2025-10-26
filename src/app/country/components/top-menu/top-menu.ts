640698f7c59e60e24084054412403e9721629c02
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-10-26 15:42:06 -0500

    feat: Add top menu

---
+++ src/app/country/components/top-menu/top-menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nav[country-top-menu]',
+  imports: [],
+  templateUrl: './top-menu.html',
+  host: { class: 'w-full flex justify-center' }
+})
+export class TopMenu {
+
+}
