3b8b84feb83610bc2e1c5b51fce9c927e7716ef1
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-10-28 17:16:59 -0500

    feat: Add regions

--- src/app/country/components/country-search-input/country-search-input.ts
+++ src/app/country/components/country-search-input/country-search-input.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -7,8 +7,22 @@ import { Component, input, output } from '@angular/core';
   host: { class: 'flex flex-row gap-2 mt-2' }
 })
 export class CountrySearchInput {
+  public inputValue = signal<string>('')
+  public debounceTime = input<number>(500);
   public placeholder = input<string>('Buscar');
   public value = output<string>();
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue()
+
+    const timeout = setTimeout(() => {
+      this.value.emit(value)
+    }, this.debounceTime());
+
+    onCleanup(() => {
+      clearTimeout(timeout)
+    })
+  })
 
   onSearch(value: string) {
     this.value.emit(value.trim())
--- src/app/country/pages/by-region-page/by-region-page.ts
+++ src/app/country/pages/by-region-page/by-region-page.ts
@@ -3,20 +3,31 @@ import { rxResource } from '@angular/core/rxjs-interop';
 import { of } from 'rxjs';
 
 import { CountryList } from "../../components/country-list/country-list";
-import { CountrySearchInput } from "../../components/country-search-input/country-search-input";
+import type { Region } from '../../interfaces/region.type';
 import { CountryService } from '../../services/country-service';
 
 @Component({
   selector: 'app-by-region-page',
-  imports: [CountrySearchInput, CountryList],
+  imports: [CountryList],
   templateUrl: './by-region-page.html',
 })
 export class ByRegionPage {
+  regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+    'Antarctic',
+  ];
   countryService = inject(CountryService)
-  query = signal<string>('')
+  selectedRegion = signal<Region | null>(null)
+
+  isSelected = (region: Region) => region === this.selectedRegion()
+
   // IMPORTANT: resource is experimental. It's ready for you to try, but it might change before it is stable.
   countryResource = rxResource({
-    params: () => ({ query: this.query() }),
+    params: () => ({ query: this.selectedRegion() }),
     stream: ({ params }) => {
       if (!params.query) return of([]);
 
